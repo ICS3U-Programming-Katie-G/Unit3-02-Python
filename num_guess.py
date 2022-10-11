5733fbd909617eccc299fbc49bbf830c80c687df
Kaitlin Green <kaitlin.green@stu.ocsb.ca>
2022-10-11 00:11:58 +0000

    Sorry this is late, I've been really tired lately. Here is the code to get the user's guess and check to see if it matches the constant of the correct guess.

---
+++ num_guess.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Created by: Katie G
+# Created on: October 10th, 2022
+# This program makes the user guess
+# the number that I have inputted.
+# The program will tell the user if
+# they got it right or wrong.
+import num_guess_constants
+
+
+# this function gets the user's guess and
+# checks to see if it is the correct num.
+# The function then displays the result
+# to the user.
+def main():
+    # getting the user's guess.
+    user_guess = int(input("Guess my favorite number from 0 - 9 :)"
+                           "I bet you won't get it :) "))
+
+    # check to see if user guess is correct.
+    if user_guess == num_guess_constants.CORRECT_NUM:
+        print("You got it right :) Good job :)")
+
+    # check to see if user guess is not correct.
+    if user_guess != num_guess_constants.CORRECT_NUM:
+        print("You did not get it right :( I am sad now :(")
+
+
+if __name__ == "__main__":
+    main()
---
+++ num_guess_constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Created by: Katie G
+# Created on: October 10th, 2022
+# This program contains the constants
+# for num_guess.py
+
+
+CORRECT_NUM = 7
